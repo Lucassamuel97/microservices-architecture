267dc5db3b32b11e7d73093e86f798e608598852
Lucassamuel97 <lukassamuka88@gmail.com>
2024-09-18 09:46:43 -0300

    feat: Added unit test for create_account use case

---
+++ internal/usecase/create_account/create_account_test.go
@@ -0,0 +1,73 @@
+package createaccount
+
+import (
+	"testing"
+
+	"github.com.br/Lucassamuel97/walletcore/internal/entity"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+type ClientGatewayMock struct {
+	mock.Mock
+}
+
+func (m *ClientGatewayMock) Save(client *entity.Client) error {
+	args := m.Called(client)
+	return args.Error(0)
+}
+
+func (m *ClientGatewayMock) Get(id string) (*entity.Client, error) {
+	args := m.Called(id)
+	return args.Get(0).(*entity.Client), args.Error(1)
+}
+
+type AccountGatewayMock struct {
+	mock.Mock
+}
+
+func (m *AccountGatewayMock) Save(account *entity.Account) error {
+	args := m.Called(account)
+	return args.Error(0)
+}
+
+func (m *AccountGatewayMock) FindByID(id string) (*entity.Account, error) {
+	args := m.Called(id)
+	return args.Get(0).(*entity.Account), args.Error(1)
+}
+
+func TestCreateAccountUseCase_Execute(t *testing.T) {
+	// Arrange
+	// Mock
+	client, _ := entity.NewClient("Lucas", "s@gmail")
+	clientMock := &ClientGatewayMock{}
+	clientMock.On("Get", client.ID).Return(client, nil)
+
+	accountMock := &AccountGatewayMock{}
+	accountMock.On("Save", mock.Anything).Return(nil)
+
+	uc := NewCreateAccountUseCase(accountMock, clientMock)
+
+	inputDto := CreateAccountInputDTO{
+		ClientID: client.ID,
+	}
+
+	// Act
+	output, err := uc.Execute(inputDto)
+
+	// Assert
+	assert.Nil(t, err)
+	assert.NotNil(t, output)
+
+	assert.NotEmpty(t, output.ID)
+
+	// Assert Valid UUID
+	_, parseErr := uuid.Parse(output.ID) // Verifica se o UUID é válido
+	assert.Nil(t, parseErr, "ID should be a valid UUID")
+
+	clientMock.AssertExpectations(t)
+	clientMock.AssertNumberOfCalls(t, "Get", 1)
+	accountMock.AssertExpectations(t)
+	accountMock.AssertNumberOfCalls(t, "Save", 1)
+}
