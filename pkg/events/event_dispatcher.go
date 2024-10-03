78562b31dbcc958f36c55cbe69f3b0d62e04c536
Lucassamuel97 <lukassamuka88@gmail.com>
2024-10-03 10:40:54 -0300

    feat: Add EventDispatcher and related interfaces
    This commit adds the EventDispatcher struct and related interfaces to the events package. The EventDispatcher is responsible for managing event handlers and dispatching events to the registered handlers. It also includes unit tests for the EventDispatcher.

---
+++ pkg/events/event_dispatcher.go
@@ -0,0 +1,70 @@
+package events
+
+import "errors"
+
+var ErrHandlerAlreadyRegistered = errors.New("handler already registered")
+
+type EventDispatcher struct {
+	handlers map[string][]EventHandlerInterface
+}
+
+func NewEventDispatcher() *EventDispatcher {
+	return &EventDispatcher{
+		handlers: make(map[string][]EventHandlerInterface),
+	}
+}
+
+func (ed *EventDispatcher) Dispatch(event EventInterface) error {
+	if handlers, ok := ed.handlers[event.GetName()]; ok {
+		for _, handler := range handlers {
+			handler.Handle(event)
+		}
+	}
+
+	return nil
+}
+
+func (ed *EventDispatcher) Register(eventName string, handler EventHandlerInterface) error {
+
+	if _, ok := ed.handlers[eventName]; ok {
+		for _, h := range ed.handlers[eventName] {
+			if h == handler {
+				return ErrHandlerAlreadyRegistered
+			}
+		}
+	}
+
+	ed.handlers[eventName] = append(ed.handlers[eventName], handler)
+	return nil
+}
+
+func (ed *EventDispatcher) Has(eventName string, handler EventHandlerInterface) bool {
+	if _, ok := ed.handlers[eventName]; ok {
+		for _, h := range ed.handlers[eventName] {
+			if h == handler {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func (ed *EventDispatcher) Remove(eventName string, handler EventHandlerInterface) error {
+	// Verifica se o evento foi registrado
+	if _, ok := ed.handlers[eventName]; ok {
+		// Itera sobre os handlers do evento
+		for i, h := range ed.handlers[eventName] {
+			// Verifica se o handler é o mesmo
+			if h == handler {
+				// Remove o handler do slice
+				ed.handlers[eventName] = append(ed.handlers[eventName][:i], ed.handlers[eventName][i+1:]...)
+				return nil
+			}
+		}
+	}
+	return nil
+}
+
+func (ed *EventDispatcher) Clear() {
+	ed.handlers = make(map[string][]EventHandlerInterface)
+}
