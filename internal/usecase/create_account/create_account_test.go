3ca72922913c12445f2f4e7bad2cd9e0045c6cba
Lucassamuel97 <lukassamuka88@gmail.com>
2024-10-07 16:12:29 -0300

    refactor: update tests create_account using mock

--- internal/usecase/create_account/create_account_test.go
+++ internal/usecase/create_account/create_account_test.go
@@ -4,52 +4,20 @@ import (
 	"testing"
 
 	"github.com.br/Lucassamuel97/walletcore/internal/entity"
+	"github.com.br/Lucassamuel97/walletcore/internal/usecase/mocks"
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 )
 
-type ClientGatewayMock struct {
-	mock.Mock
-}
-
-func (m *ClientGatewayMock) Save(client *entity.Client) error {
-	args := m.Called(client)
-	return args.Error(0)
-}
-
-func (m *ClientGatewayMock) Get(id string) (*entity.Client, error) {
-	args := m.Called(id)
-	return args.Get(0).(*entity.Client), args.Error(1)
-}
-
-type AccountGatewayMock struct {
-	mock.Mock
-}
-
-func (m *AccountGatewayMock) Save(account *entity.Account) error {
-	args := m.Called(account)
-	return args.Error(0)
-}
-
-func (m *AccountGatewayMock) FindByID(id string) (*entity.Account, error) {
-	args := m.Called(id)
-	return args.Get(0).(*entity.Account), args.Error(1)
-}
-
-func (m *AccountGatewayMock) UpdateBalance(account *entity.Account) error {
-	args := m.Called(account)
-	return args.Error(0)
-}
-
 func TestCreateAccountUseCase_Execute(t *testing.T) {
 	// Arrange
 	// Mock
 	client, _ := entity.NewClient("Lucas", "s@gmail")
-	clientMock := &ClientGatewayMock{}
+	clientMock := &mocks.ClientGatewayMock{}
 	clientMock.On("Get", client.ID).Return(client, nil)
 
-	accountMock := &AccountGatewayMock{}
+	accountMock := &mocks.AccountGatewayMock{}
 	accountMock.On("Save", mock.Anything).Return(nil)
 
 	uc := NewCreateAccountUseCase(accountMock, clientMock)
