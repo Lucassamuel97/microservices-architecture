25bbe9e17f9015e039b3b491b438f984f2281cb8
Lucassamuel97 <lukassamuka88@gmail.com>
2024-10-10 09:43:21 -0300

    feat: Adds init.sql file for database initialization

---
+++ database/init.sql
@@ -0,0 +1,35 @@
+CREATE DATABASE IF NOT EXISTS wallet;
+CREATE DATABASE IF NOT EXISTS ms_balance;
+
+USE wallet;
+
+CREATE TABLE IF NOT EXISTS `clients` (
+  `id` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `created_at` date DEFAULT NULL
+);
+
+CREATE TABLE IF NOT EXISTS `accounts` (
+  `id` varchar(255) DEFAULT NULL,
+  `client_id` varchar(255) DEFAULT NULL,
+  `balance` float DEFAULT NULL,
+  `created_at` date DEFAULT NULL
+);
+
+CREATE TABLE IF NOT EXISTS `transactions` (
+  `id` varchar(255) DEFAULT NULL,
+  `account_id_from` varchar(255) DEFAULT NULL,
+  `account_id_to` varchar(255) DEFAULT NULL,
+  `amount` float DEFAULT NULL,
+  `created_at` date DEFAULT NULL
+);
+
+
+USE balance;
+
+CREATE TABLE IF NOT EXISTS `balance` (
+  `id` varchar(255) DEFAULT NULL,
+  `account_id` varchar(255) DEFAULT NULL,
+  `amount` int(11) DEFAULT NULL
+);
